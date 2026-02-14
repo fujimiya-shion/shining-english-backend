26245ff129d06b33fd19a8cd56e526e50f2796e5
Fujimiya Shion <tranthangloi.dev@gmail.com>
2026-02-14 22:32:51 +0700

    feat(auth): init service & repo for auth module

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,12 +8,16 @@ use App\Repositories\Lesson\ILessonRepository;
 use App\Repositories\Lesson\LessonRepository;
 use App\Repositories\Quiz\IQuizRepository;
 use App\Repositories\Quiz\QuizRepository;
+use App\Repositories\User\IUserRepository;
+use App\Repositories\User\UserRepository;
 use App\Services\Course\CourseService;
 use App\Services\Course\ICourseService;
 use App\Services\Lesson\ILessonService;
 use App\Services\Lesson\LessonService;
 use App\Services\Quiz\IQuizService;
 use App\Services\Quiz\QuizService;
+use App\Services\User\IUserService;
+use App\Services\User\UserService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,10 +30,12 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(ICourseRepository::class, CourseRepository::class);
         $this->app->bind(ILessonRepository::class, LessonRepository::class);
         $this->app->bind(IQuizRepository::class, QuizRepository::class);
+        $this->app->bind(IUserRepository::class, UserRepository::class);
 
         $this->app->bind(ICourseService::class, CourseService::class);
         $this->app->bind(ILessonService::class, LessonService::class);
         $this->app->bind(IQuizService::class, QuizService::class);
+        $this->app->bind(IUserService::class, UserService::class);
     }
 
     /**
