77f1f27830434b7cf86bce1ef1bf687fce7028dc
Fujimiya Shion <tranthangloi.dev@gmail.com>
2026-02-16 11:00:25 +0700

    feat(user-cms): init filament resource and policies

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                TextInput::make('phone')
+                    ->tel()
+                    ->required(),
+                DatePicker::make('birthday'),
+                TextInput::make('avatar'),
+                Select::make('city_id')
+                    ->relationship('city', 'name'),
+                DateTimePicker::make('email_verified_at'),
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+            ]);
+    }
+}
