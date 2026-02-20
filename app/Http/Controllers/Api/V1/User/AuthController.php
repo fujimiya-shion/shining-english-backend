8e8d65365f30c295998a19ab4ff738d066b2ca15
Fujimiya Shion <tranthangloi.dev@gmail.com>
2026-02-20 21:35:15 +0700

    feat: init constructor

--- app/Http/Controllers/Api/V1/User/AuthController.php
+++ app/Http/Controllers/Api/V1/User/AuthController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Api\V1\User;
 
+use App\Http\Controllers\Api\ApiController;
 use App\Http\Controllers\Controller;
+use App\Services\User\IUserService;
 use Illuminate\Http\Request;
 
-class AuthController extends Controller
-{
-    //
+class AuthController extends ApiController {
+    public function __construct(
+        private IUserService $service
+    ) {}
+
+    
 }
